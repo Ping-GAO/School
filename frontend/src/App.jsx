406bec50d0f898de7f255b60f101c3142a147a12
Ping GAO <ping.gao@student.unsw.edu.au>
2020-12-20 19:20:47 +0800

    set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+    BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PlaceHolder from './pages/PlaceHolder';
+import SearchAppBar from './components/SearchAppBar';
 
 function App() {
     return (
         <div className="App">
-            <div>ok</div>
+            <Router>
+                <SearchAppBar />
+                <Switch>
+                    <Route exact path="/placeholder">
+                        <PlaceHolder />
+                    </Route>
+                    <Route exact path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Redirect exact from="/" to="dashboard" />
+                </Switch>
+            </Router>
         </div>
     );
 }
