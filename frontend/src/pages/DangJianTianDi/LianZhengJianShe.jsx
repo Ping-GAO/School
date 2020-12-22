15849f763daee5067f5215499bcff4afd0419a6f
Ping GAO <ping.gao@student.unsw.edu.au>
2020-12-22 22:54:50 +0800

    work

--- frontend/src/pages/DangJianTianDi/LianZhengJianShe.jsx
+++ frontend/src/pages/DangJianTianDi/LianZhengJianShe.jsx
@@ -40,6 +40,11 @@ const useStyles = makeStyles((theme) => ({
         width: 20,
         height: 20,
     },
+    mainWrap: {
+        margin: '30px 200px 0px 70px',
+        height: 800,
+        backgroundColor: 'white',
+    },
 }));
 const LianZhengJianShe = () => {
     const classes = useStyles();
@@ -69,41 +74,8 @@ const LianZhengJianShe = () => {
                 </div>
 
             </div>
-            <div style={{ height: '500px' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nullam orci nunc, auctor et augue tempus, varius fermentum erat.
-                Maecenas euismod pellentesque diam et sollicitudin. Integer
-                interdum enim tincidunt magna euismod, faucibus tincidunt erat
-                auctor. In hendrerit elit lacus, non euismod turpis dictum ac.
-                Sed elementum venenatis pharetra. Etiam neque nibh, pellentesque sed
-                interdum vitae, faucibus quis lacus. Nulla a massa sit amet arcu blandit
-                vulputate. Mauris tempor, ex sit amet consectetur porta, sapien dui sodales
-                lorem, molestie porttitor purus enim in est. Donec id porttitor dolor.
-
-                Nulla iaculis velit vitae auctor ornare. Nam non dui imperdiet,
-                malesuada est at, feugiat ante. Pellentesque habitant morbi tristique
-                senectus et netus et malesuada fames ac turpis egestas. Sed sed lacinia
-                quam, quis feugiat mi. Curabitur purus mi, euismod a blandit quis, commodo
-                sed nisl. Morbi lectus turpis, luctus at porta vel, sollicitudin ac urna.
-                Nulla mattis, ipsum vitae vestibulum posuere, nunc sapien vestibulum dui,
-                et pulvinar turpis metus a nulla. Etiam rutrum tempus mauris, sit amet tristique
-                purus commodo nec. Sed nec fermentum justo. Proin dignissim risus quam, a maximus
-                leo pharetra eget. Phasellus nisl enim, faucibus a viverra at, semper sed magna.
-                Praesent lectus velit, imperdiet eget erat at, placerat condimentum erat.
-                Etiam consectetur, metus quis hendrerit hendrerit, arcu quam aliquam erat,
-                a lacinia risus elit in odio. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                Proin vel ullamcorper lorem, et tincidunt sem.
-
-                Curabitur egestas auctor neque vel commodo. Donec in tristique mi.
-                Nunc blandit ante sit amet urna lacinia, quis interdum mauris ultricies.
-                Nam laoreet tincidunt arcu, vel dictum justo viverra id. Praesent consectetur
-                eget mi consectetur rutrum. Aenean at arcu sed purus pretium hendrerit.
-                Proin sem nulla, ullamcorper eget magna ut, aliquet accumsan mauris.
-                Suspendisse elit tortor, elementum ac aliquet ac, fringilla id ipsum.
-                Suspendisse fringilla, lectus eu porta consequat, dui odio ornare mauris,
-                et rutrum nisl neque sit amet leo. Suspendisse fringilla justo id libero maximus,
-                a convallis tortor viverra. Integer non sagittis purus.
-
+            <div className={classes.mainWrap}>
+                aaa
             </div>
         </>
     );
